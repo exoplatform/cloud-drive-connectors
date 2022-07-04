780444c755d5b3ecae637dc170f2968fe5cb5aad
Ahmed Ben Mbarek <abenmbarek@exoplatform.com>
2022-07-04 10:41:45 +0100

    Task-58742: Initialize project

---
+++ cloud-drive-connectors-webapp/webpack.prod.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
+
+const config = {
+  context: path.resolve(__dirname, '.'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ESLintPlugin({
+      files: [
+        './src/main/webapp/vue-app/*.js',
+        './src/main/webapp/vue-app/*.vue',
+        './src/main/webapp/vue-app/**/*.js',
+        './src/main/webapp/vue-app/**/*.vue',
+      ],
+    }),
+    new VueLoaderPlugin()
+  ],
+  entry: {
+    cloudDriveConnectors: './src/main/webapp/vue-app/cloud-drive-connectors/main.js',
+  },
+  output: {
+    path: path.join(__dirname, 'target/cloud-drive-connectors/'),
+    filename: 'js/[name].bundle.js',
+    libraryTarget: 'amd'
+  },
+  externals: {
+    vue: 'Vue',
+    vuetify: 'Vuetify',
+    jquery: '$',
+  },
+};
+
+module.exports = config;
---
+++ cloud-drive-connectors-webapp/webpack.watch.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+
+const webpackProductionConfig = require('./webpack.prod.js');
+
+// the display name of the war
+const app = 'cloud-drive-connectors';
+
+// add the server path to your server location path
+
+const exoServerPath = "/exo-server";
+
+
+module.exports = merge(webpackProductionConfig, {
+  output: {
+    path: path.resolve(`${exoServerPath}/webapps/${app}/`)
+  },
+  mode: 'development',
+  devtool: 'inline-source-map'
+})
