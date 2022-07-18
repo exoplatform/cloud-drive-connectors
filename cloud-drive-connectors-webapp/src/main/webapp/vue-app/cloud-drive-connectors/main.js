08c71543d38d4d95a9c33c55587aef63ae9b270d
Ahmed Ben Mbarek <92788696+abenmbarek@users.noreply.github.com>
2022-07-18 12:54:01 +0100

    Task-58742: Display dynamically onedrive and gdrive connectors (#3)
    Prior to this change, we display statically onedrive and gdrive connectors directly from ECMS codebase. After this commit we ensure that :
    
    - ECMS codebase implements only the cloud drive connectors framework with the cloud drive setting drawer portlet
    - Cloud-drive-connectors addon codebase implements onedrive and gdrive connectors and injects them into the cloud drive setting drawer portlet.

--- cloud-drive-connectors-webapp/src/main/webapp/vue-app/cloud-drive-connectors/main.js
+++ cloud-drive-connectors-webapp/src/main/webapp/vue-app/cloud-drive-connectors/main.js
@@ -14,6 +14,13 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+const connectorsImages = [
+  {id: 'gdrive', path: '/cloud-drive-connectors/skin/images/Google Drive.png'},
+  {id: 'onedrive', path: '/cloud-drive-connectors/skin/images/One Drive.png'}
+];
+extensionRegistry.registerExtension('cloud-drive-connectors', 'images', connectorsImages);
+
+
 // get overridden components if exists
 if (extensionRegistry) {
   const components = extensionRegistry.loadComponents('CloudDriveConnectors');
