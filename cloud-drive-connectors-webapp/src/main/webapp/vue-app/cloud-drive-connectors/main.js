9760f74e047fdf47e797140ed8b07cc354ebf7ae
Ayoub Zayati <azayati@exoplatform.com>
2024-03-04 22:36:05 +0100

    feat: Remove clouddrive-connectors portlet from bottom-all-container configuration - EXO-65110

--- cloud-drive-connectors-webapp/src/main/webapp/vue-app/cloud-drive-connectors/main.js
+++ cloud-drive-connectors-webapp/src/main/webapp/vue-app/cloud-drive-connectors/main.js
@@ -1,34 +1,24 @@
 /*
- * Copyright (C) 2022 eXo Platform SAS.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-const connectorsImages = [
-  {id: 'gdrive', path: '/cloud-drive-connectors/skin/images/Google Drive.png'},
-  {id: 'onedrive', path: '/cloud-drive-connectors/skin/images/One Drive.png'}
-];
-extensionRegistry.registerExtension('cloud-drive-connectors', 'images', connectorsImages);
+  Copyright (C) 2024 eXo Platform SAS.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
-// get overridden components if exists
-if (extensionRegistry) {
-  const components = extensionRegistry.loadComponents('CloudDriveConnectors');
-  if (components && components.length > 0) {
-    components.forEach(cmp => {
-      Vue.component(cmp.componentName, cmp.componentOptions);
-    });
-  }
+export function init() {
+  const connectorsImages = [
+    {id: 'gdrive', path: '/cloud-drive-connectors/skin/images/Google Drive.png'},
+    {id: 'onedrive', path: '/cloud-drive-connectors/skin/images/One Drive.png'}
+  ];
+  extensionRegistry.registerExtension('cloud-drive-connectors', 'images', connectorsImages);
 }
-
-Vue.use(Vuetify);
